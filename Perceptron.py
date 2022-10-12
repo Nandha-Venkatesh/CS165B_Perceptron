38ed1f5caf8ed18f26c6812276d4604954b4a89f
unknown <nvenkatesh@ucsb.edu>
2022-10-12 12:32:42 -0700

    Updated!

--- Perceptron.py
+++ Perceptron.py
@@ -1,2 +1,26 @@
 import numpy as np
-import scipy as sp
+import matplotlib.pyplot as plt
+
+print("Hello World!")
+
+# Generate Linearly Separable Data
+
+# Generate a line
+
+def generateLine():
+    c1 = 1;
+    c2 = 0;
+    return (c1, c2);
+
+# Generate some points above and below it. There should be 20 points total.
+
+def generatePoints():
+    pointList = [];
+    return pointList;
+
+# Perceptron Algorithm goes here.
+
+
+
+# Plot everything.
+
